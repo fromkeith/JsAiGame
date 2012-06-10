903ed03ef26f9612f3085080327dacd88684f839
fromkeith <keith@fromkeith.com>
2012-06-09 17:25:13 -0700

    Better navigation smoothing. Agent is better at not hitting walls.

--- ai/navigation.js
+++ ai/navigation.js
@@ -1,83 +1,125 @@
 define(["geom/geom", "geom/polygon", "geom/quadTree"], function (geometry, Polygon, QuadTree) {
 	"use strict";
+
+	function displayAroundCorner(finalPath, point, lp) {
+		var edge, e, disp, j, iDist, eDist, swit, newPoints = [];
+		// we need to order the points correctly
+		for (e = 0; e < point.edges.length; e++) {
+			edge = point.edges[e];
+			if (edge.parents.length > 1) {
+				if (point === edge.points[0]) {
+					disp = edge.seg.line.direction.multiply(40);
+				} else {
+					disp = edge.seg.line.direction.multiply(-40);
+				}
+				newPoints.push({
+					x : (point.x + disp.elements[0]),
+					y : (point.y + disp.elements[1]),
+					displaced : Sylvester.Line.Segment.create(
+						[point.x, point.y],
+						[point.x + disp.elements[0], point.y + disp.elements[1]]
+					)
+				});
+			}
+		}
+		// order the points
+		for (j = 0; j < newPoints.length; j++) {
+			iDist =
+				(newPoints[j].x - lp.x) * (newPoints[j].x - lp.x) +
+				(newPoints[j].y - lp.y) * (newPoints[j].y - lp.y);
+			for (e = 0; e < newPoints.length; e++) {
+				if (e === j) {
+					continue;
+				}
+				eDist =
+					(newPoints[e].x - lp.x) * (newPoints[e].x - lp.x) +
+					(newPoints[e].y - lp.y) * (newPoints[e].y - lp.y);
+				if (eDist > iDist) {
+					swit = newPoints[j];
+					newPoints[j] = newPoints[e];
+					newPoints[e] = swit;
+				}
+			}
+		}
+		// add the new points to the final path
+		for (j = 0; j < newPoints.length; j++) {
+			finalPath.push(newPoints[j]);
+		}
+	}
+	function getColor(iteration) {
+		switch (iteration) {
+		case 0:
+			return "#F00";
+		case 1:
+			return "#0F0";
+		case 2:
+			return "#00F";
+		default:
+			return "#FFF";
+		}
+	}
+	function needsDisplacement(point) {
+		var e, edge;
+		if (!point.edges) {
+			return false;
+		}
+		for (e = 0; e < point.edges.length; e++) {
+			edge = point.edges[e];
+			// means i share an edge with a wall
+			if (edge.parents.length === 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+	function displaceAllPoints(path) {
+		var i, finalPath = [];
+		finalPath.push(path[0]);
+		for (i = 1; i < path.length-1; i++) {
+			if (needsDisplacement(path[i])) {
+				displayAroundCorner(finalPath, path[i], finalPath[finalPath.length - 1]);
+			} else {
+				finalPath.push(path[i]);
+			}
+		}
+		finalPath.push(path[path.length - 1]);
+		return finalPath;
+	}
+	/**
+	 * Smooths the path, given the set of obsticles.
+	 */
+	function smoothNavigation(obsticles, path, iterations) {
+		var finalPath = [path[0]], i, ray, ob, edge, lp;
+		//Crafty.e("PolyDrawer").poly(path.concat([]), getColor(iterations));
+		for (i = 1; i < path.length; i++) {
+			lp = finalPath[finalPath.length - 1];
+			ray = Sylvester.Line.Segment.create([lp.x, lp.y], [path[i].x, path[i].y]);
+			ob = obsticles.ray(ray);
+			// if there is an obstical, we need to use the previous point
+			if (ob.length !== 0) {
+				finalPath.push(path[i - 1]);
+			} else if (path[i - 1].displaced) {
+				// if the previous point was displaced, we need to make sure this line won't get too close
+				// to the wall
+				if (ray.intersects(path[i - 1].displaced) || path[i - 1].displaced.intersects(ray)) {
+					finalPath.push(path[i - 1]);
+				}
+			}
+		}
+		finalPath.push(path[path.length - 1]);
+		if (iterations > 0 && finalPath.length !== path.length) {
+			return smoothNavigation(obsticles, finalPath, iterations - 1);
+		}
+		return finalPath;
+	}
+	function tuneNavigation(obsticles, path, iterations) {
+		//Crafty.e("PolyDrawer").poly(path.concat([]), getColor(iterations + 1));
+		return smoothNavigation(obsticles, displaceAllPoints(path), iterations);
+	}
+
 	return {
 		navTree : null,
 		obsticles : null,
-		/**
-		 * Smooths the path, given the set of obsticles.
-		 */
-		smoothNavigation : function (obsticles, path, iterations) {
-			var finalPath = [path[0]], i, j, ray, ob, displaced, e, edge, disp, needDisplace, newPoints,
-				iDist, eDist, swit, lp;
-			for (i = 2; i < path.length; i++) {
-				lp = finalPath[finalPath.length - 1];
-				ray = Sylvester.Line.Segment.create([lp.x, lp.y], [path[i].x, path[i].y]);
-				ob = obsticles.ray(ray);
-				// if there is an obstical, we need to use the previous point
-				if (ob.length !== 0) {
-					displaced = false;
-					if (path[i - 1].edges) {
-						needDisplace = false;
-						for (e = 0; e < path[i - 1].edges.length; e++) {
-							edge = path[i - 1].edges[e];
-							// means i share an edge with a wall
-							if (edge.parents.length === 1) {
-								needDisplace = true;
-								break;
-							}
-						}
-						if (needDisplace) {
-							// we need to order the points correctly
-							newPoints = [];
-							for (e = 0; e < path[i - 1].edges.length; e++) {
-								edge = path[i - 1].edges[e];
-								if (edge.parents.length > 1) {
-									if (path[i - 1] === edge.points[0]) {
-										disp = edge.seg.line.direction.multiply(40);
-									} else {
-										disp = edge.seg.line.direction.multiply(-40);
-									}
-									newPoints.push({x : (path[i - 1].x + disp.elements[0]), y : (path[i - 1].y + disp.elements[1])});
-									displaced = true;
-								}
-							}
-							for (j = 0; j < newPoints.length; j++) {
-								iDist =
-									(newPoints[j].x - lp.x) * (newPoints[j].x - lp.x) +
-									(newPoints[j].y - lp.y) * (newPoints[j].y - lp.y);
-								for (e = 0; e < newPoints.length; e++) {
-									if (e === i) {
-										continue;
-									}
-									eDist =
-										(newPoints[e].x - lp.x) * (newPoints[e].x - lp.x) +
-										(newPoints[e].y - lp.y) * (newPoints[e].y - lp.y);
-									if (eDist > iDist) {
-										swit = newPoints[j];
-										newPoints[j] = newPoints[e];
-										newPoints[e] = swit;
-									}
-								}
-							}
-							for (j = 0; j < newPoints.length; j++) {
-								finalPath.push(newPoints[j]);
-							}
-						}
-					}
-					if (!displaced) {
-						finalPath.push(path[i-1]);
-					}
-				}
-				// TODO:
-				// look at points/edges, and translate them away from walls, 90 degrees (edges are CW)
-				// when removing points, check to make sure line isn't closer than translated point
-			}
-			finalPath.push(path[path.length - 1]);
-			if (iterations > 0 && finalPath.length !== path.length) {
-				return this.smoothNavigation(obsticles, finalPath, iterations - 1);
-			}
-			return finalPath;
-		},
 
 		hashPoint : function (point) {
 			return "x:" + point.x + "y:" + point.y;
@@ -151,7 +193,7 @@ define(["geom/geom", "geom/polygon", "geom/quadTree"], function (geometry, Polyg
 				if (endParent.contains(curPoint)) {
 					// reconstruct path
 					if (cameFrom[curPointHash]) {
-						return this.smoothNavigation(this.obsticles,
+						return tuneNavigation(this.obsticles,
 							[to, curPoint].concat(this.reconstruct(cameFrom, cameFrom[curPointHash])), 2);
 					} else {
 						return [to, curPoint];
